56af2dffd73fa358d2b5c8b2ff268589dfa6d26f
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-04-06 23:47:02 -0300

    [BUILD] Criado a representação em Java de Funcionario.

--- src/main/java/com/lemnos/server/Models/Endereco.java
+++ src/main/java/com/lemnos/server/Models/Endereco.java
@@ -27,4 +27,8 @@ public class Endereco {
     @JsonIgnore
     @ManyToMany(mappedBy = "enderecos")
     private List<Cliente> clientes;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "enderecos")
+    private List<Funcionario> funcionarios;
 }
---
+++ src/main/java/com/lemnos/server/Models/Funcionario.java
@@ -0,0 +1,43 @@
+package com.lemnos.server.Models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Table(name = "Funcionario")
+@Data
+public class Funcionario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Integer Id;
+
+    @Column(name = "Nome")
+    private String Nome;
+
+    @Column(name = "CPF", unique = true)
+    private String CPF;
+
+    @Column(name = "Data_Nascimento")
+    private Date DataNascimento;
+
+    @Column(name = "Data_Admissao")
+    private Date DataAdmissao;
+
+    @Column(name = "Telefone")
+    private String Telefone;
+
+    @Column(name = "CEP")
+    private String CEP;
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "Funcionario_Possui_Endereco",
+            joinColumns = @JoinColumn(name = "Id_Funcionario"),
+            inverseJoinColumns = @JoinColumn(name = "Id_Endereco")
+    )
+    private List<Endereco> enderecos;
+}
