3a914a183481a8f8c1d2bdc2c3d6ddd290983a26
leankk <leandrofamiliafox@gmail.com>
2024-04-10 01:36:08 -0300

    [BUILD] Criado o manejamento de cadastro de fornecedores, com sua classe FornecedorDTO e suas respectivas validações.

---
+++ src/main/java/com/lemnos/server/Repositories/FornecedorRepository.java
@@ -0,0 +1,11 @@
+package com.lemnos.server.Repositories;
+
+import com.lemnos.server.Models.Fornecedor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer> {
+
+    Optional<Fornecedor> findByCnpj(String string);
+}
