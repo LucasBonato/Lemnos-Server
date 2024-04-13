e0a63d8ae88bf2268ec92c7958b04ab04289e808
leankk <leandrofamiliafox@gmail.com>
2024-04-13 03:04:27 -0300

    [BUILD] Criado DELETE para cliente.
    Criado o método delete para o cliente utilizando seu Id.

--- src/main/java/com/lemnos/server/Controllers/ClienteController.java
+++ src/main/java/com/lemnos/server/Controllers/ClienteController.java
@@ -4,10 +4,7 @@ import com.lemnos.server.Models.Cliente;
 import com.lemnos.server.Services.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +22,10 @@ public class ClienteController {
     public ResponseEntity<Cliente> getOneById(@PathVariable Integer id){
         return clienteService.getOneById(id);
     }
+
+//    @PutMapping("/{id}")
+//    public ResponseEntity<Cliente> updateById(@PathVariable Integer id){ return clienteService.updateClient(Cliente, id); }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Cliente> deleteById(@PathVariable Integer id){ return clienteService.deleteById(id);}
 }
--- src/main/java/com/lemnos/server/Services/ClienteService.java
+++ src/main/java/com/lemnos/server/Services/ClienteService.java
@@ -2,9 +2,12 @@ package com.lemnos.server.Services;
 
 import com.lemnos.server.Models.Cliente;
 import com.lemnos.server.Repositories.ClienteRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,5 +27,29 @@ public class ClienteService {
             return ResponseEntity.ok(clienteOptional.get());
         }
         throw new RuntimeException("Cliente não encontrado");
+    }
+
+//    public ResponseEntity<Cliente> updateClient(Cliente cliente, Integer id){
+//        Optional<Cliente> clienteOptional = clienteRepository.findById(id);
+//        if(clienteOptional.isPresent()){
+//            Cliente updatedCliente = getOneById(cliente.getId()).getBody();
+//            if (updatedCliente != null) {
+//                updatedCliente.setNome(cliente.getNome());
+//                updatedCliente.setCpf(cliente.getCpf());
+//                updatedCliente.setCep(cliente.getCep());
+//                updatedCliente.setNumeroLogradouro(cliente.getNumeroLogradouro());
+//                return ResponseEntity.ok(updatedCliente);
+//            }
+//        }
+//        throw new RuntimeException("Não foi possível atualizar os dados do cliente");
+//    }
+    public ResponseEntity<Cliente> deleteById(Integer id){
+        try{
+            clienteRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception ex){
+            throw new RuntimeException("Não foi possível deletar o cliente!");
+        }
     }
 }
