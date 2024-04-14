e7ee4467ff76eba82d8325b4c0c3c5eab79e4d3a
leankk <leandrofamiliafox@gmail.com>
2024-04-13 22:30:07 -0300

    [BUILD] Criado o método DELETE para o Funcionario
    Criado o procedimento para deletar o funcionário a partir do Id e arrumado um bug na hora de cadastra-lo, que estava passando os campos da tabela Cadastro como null

--- src/main/java/com/lemnos/server/Controllers/FuncionarioController.java
+++ src/main/java/com/lemnos/server/Controllers/FuncionarioController.java
@@ -4,16 +4,13 @@ import com.lemnos.server.Models.Funcionario;
 import com.lemnos.server.Services.FuncionarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/funcionario")
-public class FuncionarioController {
+class FuncionarioController {
 
     @Autowired private FuncionarioService funcionarioService;
 
@@ -25,5 +22,10 @@ public class FuncionarioController {
     @GetMapping("/{id}")
     public ResponseEntity<Funcionario> getOneById(@PathVariable Integer id){
         return funcionarioService.getOneById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Funcionario> deleteById(@PathVariable Integer id){
+        return funcionarioService.deleteById(id);
     }
 }
