a92755c258d7f3fa65949aeeefe9b656c5d38cd1
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-06-01 01:32:21 -0300

    [FIX] Retirando findByCadastro

--- src/main/java/com/lemnos/server/repositories/entidades/FornecedorRepository.java
+++ src/main/java/com/lemnos/server/repositories/entidades/FornecedorRepository.java
@@ -1,11 +1,9 @@
 package com.lemnos.server.repositories.entidades;
 
-import com.lemnos.server.models.cadastro.Cadastro;
 import com.lemnos.server.models.entidades.Fornecedor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.swing.text.html.Option;
 import java.util.Optional;
 
 @Repository
@@ -14,6 +12,4 @@ public interface FornecedorRepository extends JpaRepository<Fornecedor, Integer>
     Optional<Fornecedor> findByEmail(String email);
 
     Optional<Fornecedor> findByNome(String fornecedor);
-
-    Optional<Fornecedor> findByCadastro(Cadastro cadastro);
 }
