1215e93a1845e3b55769955abc21510af68105f1
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-06-07 17:31:04 -0300

    [REFACTOR] Trocando o aquivo JSON por variavel de ambiente

--- src/main/java/com/lemnos/server/configurations/security/FireBaseConfiguration.java
+++ src/main/java/com/lemnos/server/configurations/security/FireBaseConfiguration.java
@@ -3,23 +3,32 @@ package com.lemnos.server.configurations.security;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import com.lemnos.server.exceptions.BaseException;
+import com.lemnos.server.exceptions.ExceptionResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.HttpStatus;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 @Configuration
 public class FireBaseConfiguration {
+    @Value("${firebase.credentials.service.account.key}")
+    private String credentials;
+
     @Bean
-    public FirebaseApp initialize() throws IOException {
-        ClassPathResource resource = new ClassPathResource("serviceAccountKey.json");
-        InputStream serviceAccount = resource.getInputStream();
+    public FirebaseApp initialize() throws IOException, IllegalAccessException {
+        if(credentials == null) {
+            throw new BaseException(HttpStatus.NOT_FOUND, new ExceptionResponse(0, "Não possui as credenciais"));
+        }
+
+        InputStream serviceAccount = new ByteArrayInputStream(credentials.getBytes());
 
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .setDatabaseUrl("")
                 .build();
 
         if (FirebaseApp.getApps().isEmpty()) {
