8b06798a6cfa9826f1ea4cdcbe8063ec5f5e4d27
Lucas Bonato <lucas.perez.bonato@gmail.com>
2024-11-18 19:06:20 -0300

    [BUILD] Using FlyWay to set some constants db values

--- src/main/java/com/lemnos/server/configurations/security/AdminConfiguration.java
+++ src/main/java/com/lemnos/server/configurations/security/AdminConfiguration.java
@@ -24,22 +24,22 @@ public class AdminConfiguration implements CommandLineRunner {
     public void run(String... args) throws Exception {
         Optional<Cadastro> adminOptional = cadastroRepository.findByEmail("admin@gmail.com");
         adminOptional.ifPresentOrElse(
-                user -> {
-                    System.out.println("Admin já existe!");
-                },
-                () -> {
-                    Funcionario admin = new Funcionario();
-                    admin.setCadastro(new Cadastro("admin@gmail.com", passwordEncoder.encode("admin")));
-                    admin.setRole(Roles.ADMIN);
-                    admin.setCpf(23504391864L);
-                    admin.setTelefone(11972540380L);
-                    admin.setNome("admin");
-                    admin.setDataAdmissao(Util.convertData("16/05/2024"));
-                    admin.setDataNascimento(Util.convertData("01/01/2000"));
+            user -> {
+                System.out.println("Admin já existe!");
+            },
+            () -> {
+                Funcionario admin = new Funcionario();
+                admin.setCadastro(new Cadastro("admin@gmail.com", passwordEncoder.encode("admin")));
+                admin.setRole(Roles.ADMIN);
+                admin.setCpf(23504391864L);
+                admin.setTelefone(11972540380L);
+                admin.setNome("admin");
+                admin.setDataAdmissao(Util.convertData("16/05/2024"));
+                admin.setDataNascimento(Util.convertData("01/01/2000"));
 
-                    funcionarioRepository.save(admin);
-                    System.out.println("Admin cadastrado!");
-                }
+                funcionarioRepository.save(admin);
+                System.out.println("Admin cadastrado!");
+            }
         );
 
     }
