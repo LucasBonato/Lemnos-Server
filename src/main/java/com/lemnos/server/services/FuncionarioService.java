1e44d8089c5f15246e8f756a14af5f1fa57fbc5f
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-01-17 02:13:57 -0300

    [REFACTOR] Usign streams in Employee Service

--- src/main/java/com/lemnos/server/services/FuncionarioService.java
+++ src/main/java/com/lemnos/server/services/FuncionarioService.java
@@ -7,7 +7,6 @@ import com.lemnos.server.exceptions.global.UpdateNotValidException;
 import com.lemnos.server.models.dtos.requests.FuncionarioFiltroRequest;
 import com.lemnos.server.models.dtos.requests.FuncionarioRequest;
 import com.lemnos.server.models.dtos.responses.EnderecoResponse;
-import com.lemnos.server.models.endereco.possui.FuncionarioPossuiEndereco;
 import com.lemnos.server.models.entidades.FuncionarioSpecification;
 import com.lemnos.server.models.enums.Codigo;
 import com.lemnos.server.models.enums.Situacao;
@@ -17,7 +16,7 @@ import com.lemnos.server.repositories.cadastro.CadastroRepository;
 import com.lemnos.server.repositories.entidades.FuncionarioRepository;
 import com.lemnos.server.utils.Util;
 import io.micrometer.common.util.StringUtils;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -27,36 +26,37 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 import org.springframework.stereotype.Service;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
+@RequiredArgsConstructor
 public class FuncionarioService extends Util {
-
-    @Autowired private FuncionarioRepository funcionarioRepository;
-    @Autowired private CadastroRepository cadastroRepository;
+    private final FuncionarioRepository funcionarioRepository;
+    private final CadastroRepository cadastroRepository;
 
     @Cacheable("allFuncionarios")
     public ResponseEntity<List<FuncionarioResponse>> getAll() {
-        List<Funcionario> funcionarios = funcionarioRepository.findAll();
-        List<FuncionarioResponse> dto = new ArrayList<>();
-        for(Funcionario funcionario : funcionarios){
-            dto.add(getFuncionarioResponse(funcionario));
-        }
-        return ResponseEntity.ok(dto);
+        List<FuncionarioResponse> response = funcionarioRepository.findAll()
+                .stream()
+                .map(this::getFuncionarioResponse)
+                .collect(Collectors.toList());
+        return ResponseEntity.ok(response);
     }
 
-    public ResponseEntity<List<FuncionarioResponse>> filterByName(FuncionarioFiltroRequest filtro){
+    public ResponseEntity<List<FuncionarioResponse>> filterByName(FuncionarioFiltroRequest filtro) {
         Specification<Funcionario> specification = Specification.where(null);
 
-        if(StringUtils.isNotBlank(filtro.nome())){
+        if (StringUtils.isNotBlank(filtro.nome())) {
             specification = specification.and(FuncionarioSpecification.hasNome(filtro.nome()));
         }
         int page = (filtro.page() != null && filtro.page() > 0) ? filtro.page() : 0;
         int size = (filtro.size() != null && filtro.size() > 0) ? filtro.size() : 5;
         Pageable pageable = PageRequest.of(page, size);
 
-        List<FuncionarioResponse> funcionarioResponse = new ArrayList<>();
-        funcionarioRepository.findAll(specification, pageable)
-                .forEach(funcionario -> funcionarioResponse.add(getFuncionarioResponse(funcionario)));
+        List<FuncionarioResponse> funcionarioResponse = funcionarioRepository.findAll(specification, pageable)
+                .stream()
+                .map(this::getFuncionarioResponse)
+                .collect(Collectors.toList());
 
         return ResponseEntity.ok(funcionarioResponse);
     }
@@ -73,7 +73,7 @@ public class FuncionarioService extends Util {
         return ResponseEntity.ok(record);
     }
 
-    public ResponseEntity<Void> updateFuncionario(String email, FuncionarioRequest funcionarioRequest){
+    public ResponseEntity<Void> updateFuncionario(String email, FuncionarioRequest funcionarioRequest) {
         Funcionario updatedFuncionario = insertData(email, funcionarioRequest);
         funcionarioRepository.save(updatedFuncionario);
 
@@ -81,25 +81,27 @@ public class FuncionarioService extends Util {
     }
 
     public ResponseEntity<Void> ativarOuDesativar(List<String> emails) {
-        for(String email : emails) {
-            Funcionario funcionario = getOneFuncionarioByEmail(email);
-            funcionario.setSituacao((funcionario.getSituacao().getSituacao().equals(Situacao.ATIVO.getSituacao())) ? Situacao.INATIVO : Situacao.ATIVO);
-            funcionarioRepository.save(funcionario);
-        }
+        emails.stream()
+                .map(this::getOneFuncionarioByEmail)
+                .forEach(funcionario -> {
+                    Situacao novaSituacao = funcionario.getSituacao().getSituacao().equals(Situacao.ATIVO.getSituacao()) ? Situacao.INATIVO : Situacao.ATIVO;
+                    funcionario.setSituacao(novaSituacao);
+                    funcionarioRepository.save(funcionario);
+                });
         return ResponseEntity.ok().build();
     }
 
-    public ResponseEntity<Void> deleteByEmail(String email){
+    public ResponseEntity<Void> deleteByEmail(String email) {
         Funcionario funcionarioDeletado = getOneFuncionarioByEmail(email);
 
-        if(funcionarioDeletado.getSituacao() == Situacao.ATIVO) {
+        if (funcionarioDeletado.getSituacao() == Situacao.ATIVO) {
             funcionarioDeletado.setSituacao(Situacao.INATIVO);
             funcionarioRepository.save(funcionarioDeletado);
         }
         return ResponseEntity.noContent().build();
     }
 
-    private static FuncionarioResponse getFuncionarioResponse(Funcionario funcionario) {
+    private FuncionarioResponse getFuncionarioResponse(Funcionario funcionario) {
         return new FuncionarioResponse(
                 funcionario.getNome(),
                 funcionario.getDataNascimento(),
@@ -113,61 +115,63 @@ public class FuncionarioService extends Util {
     }
 
     private void verificarToken(JwtAuthenticationToken token) {
-        if(token == null) throw new TokenNotValidOrExpiredException();
+        if (token == null) throw new TokenNotValidOrExpiredException();
     }
+
     private Funcionario getOneFuncionarioByEmail(String email) {
         return funcionarioRepository.findByCadastro(
                 cadastroRepository.findByEmail(email.replace("%40", "@")).orElseThrow(FuncionarioNotFoundException::new)
         ).orElseThrow(FuncionarioNotFoundException::new);
     }
+
     private static List<EnderecoResponse> getEnderecoRecords(Funcionario funcionario) {
-        List<EnderecoResponse> enderecoResponses = new ArrayList<>();
-        for(FuncionarioPossuiEndereco fpe : funcionario.getEnderecos()){
-            enderecoResponses.add(new EnderecoResponse(
-                    fpe.getEndereco().getCep(),
-                    fpe.getEndereco().getLogradouro(),
-                    fpe.getNumeroLogradouro(),
-                    fpe.getComplemento(),
-                    fpe.getEndereco().getCidade().getCidade(),
-                    fpe.getEndereco().getBairro(),
-                    fpe.getEndereco().getEstado().getUf()
-            ));
-        }
-        return enderecoResponses;
+        return funcionario.getEnderecos().stream()
+                .map(funcionarioEndereco -> new EnderecoResponse(
+                        funcionarioEndereco.getEndereco().getCep(),
+                        funcionarioEndereco.getEndereco().getLogradouro(),
+                        funcionarioEndereco.getNumeroLogradouro(),
+                        funcionarioEndereco.getComplemento(),
+                        funcionarioEndereco.getEndereco().getCidade().getCidade(),
+                        funcionarioEndereco.getEndereco().getBairro(),
+                        funcionarioEndereco.getEndereco().getEstado().getUf()
+                ))
+                .collect(Collectors.toList());
     }
+
     private Funcionario insertData(String email, FuncionarioRequest funcionarioEnviado) {
         Funcionario funcionarioEncontrado = getOneFuncionarioByEmail(email);
 
         Date dataNasc;
         Date dataAdmi;
 
-        if(StringUtils.isBlank(funcionarioEnviado.nome()) && StringUtils.isBlank(funcionarioEnviado.cpf()) && StringUtils.isBlank(funcionarioEnviado.dataAdmissao()) && StringUtils.isBlank(funcionarioEnviado.dataNascimento()) && StringUtils.isBlank(funcionarioEnviado.telefone())){
+        if (StringUtils.isBlank(funcionarioEnviado.nome()) && StringUtils.isBlank(funcionarioEnviado.cpf()) && StringUtils.isBlank(funcionarioEnviado.dataAdmissao()) && StringUtils.isBlank(funcionarioEnviado.dataNascimento()) && StringUtils.isBlank(funcionarioEnviado.telefone())) {
             throw new UpdateNotValidException("Funcionário");
         }
-        if(StringUtils.isBlank(funcionarioEnviado.nome())){
+        if (StringUtils.isBlank(funcionarioEnviado.nome())) {
             funcionarioEnviado = funcionarioEnviado.setNome(funcionarioEncontrado.getNome());
         }
-        if(StringUtils.isBlank(funcionarioEnviado.cpf())){
+        if (StringUtils.isBlank(funcionarioEnviado.cpf())) {
             funcionarioEnviado = funcionarioEnviado.setCpf(funcionarioEncontrado.getCpf().toString());
         }
         Long cpf = convertStringToLong(funcionarioEnviado.cpf(), Codigo.CPF);
-        if(StringUtils.isBlank(funcionarioEnviado.dataNascimento())){
+        if (StringUtils.isBlank(funcionarioEnviado.dataNascimento())) {
             dataNasc = funcionarioEncontrado.getDataNascimento();
         } else {
             dataNasc = convertData(funcionarioEnviado.dataNascimento());
         }
-        if(StringUtils.isBlank(funcionarioEnviado.dataAdmissao())){
+        if (StringUtils.isBlank(funcionarioEnviado.dataAdmissao())) {
             dataAdmi = funcionarioEncontrado.getDataAdmissao();
         } else {
             dataAdmi = convertData(funcionarioEnviado.dataAdmissao());
         }
-        if(StringUtils.isBlank(funcionarioEnviado.telefone())){
+        if (StringUtils.isBlank(funcionarioEnviado.telefone())) {
             funcionarioEnviado = funcionarioEnviado.setTelefone(funcionarioEncontrado.getTelefone().toString());
         }
         Long telefone = convertStringToLong(funcionarioEnviado.telefone(), Codigo.TELEFONE);
 
         Optional<Funcionario> funcionarioOptional = funcionarioRepository.findByCpf(cpf);
-        if(funcionarioOptional.isPresent() && !Objects.equals(funcionarioOptional.get().getId(), funcionarioEncontrado.getId())) throw new CadastroCpfAlreadyInUseException();
+        if (funcionarioOptional.isPresent() && !Objects.equals(funcionarioOptional.get().getId(), funcionarioEncontrado.getId()))
+            throw new CadastroCpfAlreadyInUseException();
 
         Funcionario updatedFuncionario = new Funcionario();
         updatedFuncionario.setId(funcionarioEncontrado.getId());
