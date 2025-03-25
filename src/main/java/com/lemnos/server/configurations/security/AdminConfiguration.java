a975c1dd34b895ad0cc78d5b4b36cbcb47597231
Lucas Bonato <lucas.perez.bonato@gmail.com>
2025-03-25 01:11:41 -0300

    [CHORE] Change admin cofigurations to use logs instead of sout

--- src/main/java/com/lemnos/server/configurations/security/AdminConfiguration.java
+++ src/main/java/com/lemnos/server/configurations/security/AdminConfiguration.java
@@ -6,6 +6,7 @@ import com.lemnos.server.models.enums.Roles;
 import com.lemnos.server.repositories.cadastro.CadastroRepository;
 import com.lemnos.server.repositories.entidades.FuncionarioRepository;
 import com.lemnos.server.shared.Convert;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -13,9 +14,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+@Slf4j
 @Configuration
 public class AdminConfiguration implements CommandLineRunner {
-
     @Autowired private FuncionarioRepository funcionarioRepository;
     @Autowired private CadastroRepository cadastroRepository;
     @Autowired private PasswordEncoder passwordEncoder;
@@ -24,7 +25,7 @@ public class AdminConfiguration implements CommandLineRunner {
     public void run(String... args) {
         Optional<Cadastro> adminOptional = cadastroRepository.findByEmail("admin@gmail.com");
         adminOptional.ifPresentOrElse(
-            user -> System.out.println("Admin já existe!"),
+            user -> log.info("[INFO] Admin já existe!"),
             () -> {
                 Funcionario admin = new Funcionario();
                 admin.setCadastro(new Cadastro("admin@gmail.com", passwordEncoder.encode("admin")));
@@ -36,7 +37,7 @@ public class AdminConfiguration implements CommandLineRunner {
                 admin.setDataNascimento(Convert.toData("01/01/2000"));
 
                 funcionarioRepository.save(admin);
-                System.out.println("Admin cadastrado!");
+                log.info("[INFO] Admin cadastrado!");
             }
         );
 
